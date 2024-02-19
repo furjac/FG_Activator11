bb223211fabd92588ac887aa1373bdfbbf547c32
Furjack <81816200+furjac@users.noreply.github.com>
2024-02-19 13:09:00 +0530

    made it easy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2023 Furjack
+Copyright (c) 2024 Furjack
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,25 +22,50 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from colorama import Fore, Style
-from activation.office import activate_office
-from activation.windows import activate_windows
 import os
 import webbrowser
-import re
-import requests
-import wget
+import subprocess
+import sys
+
 
 badge_url = "https://img.shields.io/github/v/release/furjac/FG_Activator11"
-
-def main():
-    activate_windows()
-
-def office():
-    activate_office()
 
 def clear():
     os.system('cls')
 
+def get_resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        # In development, use the script's directory
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def windows():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    path = get_resource_path(os.path.join("activation", "windows.cmd"))
+
+    subprocess.call(["cmd.exe", "/c", path], shell=True)
+
+def office():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    path = get_resource_path(os.path.join("activation", "ohook.cmd"))
+
+    subprocess.call(["cmd.exe", "/c", path], shell=True)
+
+def trouble():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    path = get_resource_path(os.path.join("activation", "troubleshoot.cmd"))
+
+    subprocess.call(["cmd.exe", "/c", path], shell=True)
+
+def change():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    path = get_resource_path(os.path.join("activation", "change_editions.cmd"))
+
+    subprocess.call(["cmd.exe", "/c", path], shell=True)
 
 # Define colors
 GREEN = Fore.GREEN
@@ -56,7 +81,9 @@ menu = f"""
             ||{"-" * 88}||
             ||          [1] {GREEN}Windows   {CYAN}                 |       [2] {GREEN}Microsoft Office{CYAN}                   ||
             ||{"-" * 88}||
-            ||          [3] {YELLOW}Donate us{CYAN}                  |       [0] {RED}Exit{CYAN}                               ||
+            ||          [3] {YELLOW}Troubleshoot{CYAN}               |       [4] {RED}Change Editions{CYAN}                    ||
+            ||{"-" * 88}||
+            ||          [5] {RED}Donate us{CYAN}                  |       [0] {RED}Exit{CYAN}                               ||
             {CYAN}{"-" * 92}
 """
 print(RESET)
@@ -66,19 +93,27 @@ while True:
 
     op = input("Please specify what you want to activate: ")
 
-    while op not in ['1', '2', '3', '0']:
+    while op not in ['1', '2', '3','4','5', '0']:
         print("Invalid option. 😒 choose a correct one")
         op = input("Please specify what you want to activate: ")
 
     if op == '1':
         clear()
-        main()
+        windows()
         input("Press Enter to continue...")
     elif op == '2':
         clear()
         office()
         input('Press enter to continue....')
     elif op == '3':
+        clear()
+        trouble()
+        input('Press enter to continue...')
+    elif op == '4':
+        clear()
+        change()
+        input('Press enter to continue...')
+    elif op == '5':
         webbrowser.open('https://fg-repacks.site/p/donate.html')
     elif op == '0':
         clear()
